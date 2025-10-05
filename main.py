c76395fd9a885a98ca08b1feb4f7a858fd30550a
asmeet28 <asmitk428@gmail.com>
2025-10-06 01:33:00 +0530

    2-

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ files = os.listdir(path)
 for file in files:
     filename, extension = os.path.splitext(file)
     extension = extension[1:]
+    
 
     if os.path.exists(path+'/'+extension):
         shutil.move(path+'/'+file, path+'/'+extension+'/'+file)
